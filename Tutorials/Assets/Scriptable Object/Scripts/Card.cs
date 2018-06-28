317aac7cb2c759a6c1ac8cbd33d23e331ca9978d
Quentin <quentin.flueckiger@hispeed.ch>
2018-06-28 15:04:30 +0200

    Scriptable object

---
+++ Tutorials/Assets/Scriptable Object/Scripts/Card.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+    
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+
+    public int manaCost;
+    public int attack;
+    public int health;
+
+    public void Print()
+    {
+        Debug.Log(name + ": " + description + " The card costs: " + manaCost);
+    }
+}
